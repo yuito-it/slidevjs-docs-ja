b3683881dab773cee81d111be74c2f8fa7d0c9fe
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-06-19 00:42:58 +0900

    ドキュメントの新しい情報を日本語に翻訳

--- .vitepress/theme/components/Layout.vue
+++ .vitepress/theme/components/Layout.vue
@@ -1,30 +1,35 @@
 <script setup lang="ts">
-import { Dropdown } from 'floating-vue'
-import { useRoute } from 'vitepress'
-import VPMenuLink from 'vitepress/dist/client/theme-default/components/VPMenuLink.vue'
-import DefaultTheme from 'vitepress/theme'
-import FeatureTag from './FeatureTag.vue'
+import { Dropdown } from "floating-vue";
+import { useRoute } from "vitepress";
+import VPMenuLink from "vitepress/dist/client/theme-default/components/VPMenuLink.vue";
+import DefaultTheme from "vitepress/theme";
+import FeatureTag from "./FeatureTag.vue";
 
-const { Layout } = DefaultTheme
-const route = useRoute()
+const { Layout } = DefaultTheme;
+const route = useRoute();
 </script>
 
 <template>
   <Layout>
     <template #nav-bar-title-after>
       <a @click.prevent>
-        <Dropdown :triggers="['hover', 'click']" :popper-triggers="['hover']" theme="twoslash" popper-class="z-1000">
-          <Badge class="scale-80 translate-x--1 select-none">
-            New Docs!
-          </Badge>
+        <Dropdown
+          :triggers="['hover', 'click']"
+          :popper-triggers="['hover']"
+          theme="twoslash"
+          popper-class="z-1000"
+        >
+          <Badge class="scale-80 translate-x--1 select-none"> New Docs! </Badge>
           <template #popper>
             <div class="p3 text-sm">
+              <p>現在ご覧いただいているのは新しいSlidevドキュメントです。</p>
               <p>
-                You are viewing the new Slidev documentation.
-              </p>
-              <p>
-                The old one is available
-                <a href="https://docs-legacy.sli.dev/" class="underline text-primary" target="_blank">here</a>.
+                旧ドキュメントサイトは<a
+                  href="https://docs-legacy.sli.dev/"
+                  class="underline text-primary"
+                  target="_blank"
+                  >こちら</a
+                >からご覧いただけます。
               </p>
             </div>
           </template>
@@ -33,18 +38,26 @@ const route = useRoute()
     </template>
     <template #sidebar-nav-after>
       <div flex="~ col gap-2">
-        <div v-if="route.data?.frontmatter?.tags" class="bg-$vp-c-bg-soft p4 rounded-lg" flex="~ col gap-2">
-          <div font-bold text-sm op75>
-            Tags
-          </div>
+        <div
+          v-if="route.data?.frontmatter?.tags"
+          class="bg-$vp-c-bg-soft p4 rounded-lg"
+          flex="~ col gap-2"
+        >
+          <div font-bold text-sm op75>Tags</div>
           <div flex="~ wrap gap-2">
-            <FeatureTag v-for="tag in route.data.frontmatter.tags" :key="tag" :tag="tag" />
+            <FeatureTag
+              v-for="tag in route.data.frontmatter.tags"
+              :key="tag"
+              :tag="tag"
+            />
           </div>
         </div>
-        <div v-if="route.data?.frontmatter?.since" class="bg-$vp-c-bg-soft px2 pb2 rounded-lg" flex="~ col gap-1">
-          <div font-bold text-sm op75 px2 pt4>
-            Since
-          </div>
+        <div
+          v-if="route.data?.frontmatter?.since"
+          class="bg-$vp-c-bg-soft px2 pb2 rounded-lg"
+          flex="~ col gap-1"
+        >
+          <div font-bold text-sm op75 px2 pt4>Since</div>
           <VPMenuLink
             :item="{
               text: route.data.frontmatter.since,
